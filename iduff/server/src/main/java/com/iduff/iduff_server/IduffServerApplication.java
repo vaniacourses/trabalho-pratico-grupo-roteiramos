b7b8df87e4b33bbc249baa759c589d3ffbdbbed8
Felipe Figueiredo Felisbino Branquinho <148362811+FelipeFFB@users.noreply.github.com>
2025-07-08 19:22:19 -0300

    Adicionando código do servidor
    Usamos o framework Spring Boot

---
+++ iduff/server/src/main/java/com/iduff/iduff_server/IduffServerApplication.java
@@ -0,0 +1,13 @@
+package com.iduff.iduff_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IduffServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IduffServerApplication.class, args);
+	}
+
+}
---
+++ iduff/server/src/test/java/com/iduff/iduff_server/IduffServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iduff.iduff_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IduffServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
